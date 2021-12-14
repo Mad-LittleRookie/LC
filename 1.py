7412233fee6946a31a2090ae609b64cb5adc2e8f
Howard.Z <90691788+Zhowards@users.noreply.github.com>
2021-12-14 18:58:40 +0800

    first debug

--- 1.py
+++ 1.py
@@ -1,1 +1,13 @@
 print("hello world")
+i = 1
+count  = 0
+count = nums[i]
+leng = len(nums)
+for length in nums
+for number in nums:
+    if (nums[count] == nums[i]) :
+        print("false")
+    i += 1
+
+
+
