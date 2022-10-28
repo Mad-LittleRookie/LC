a63c7e2acfa09a7c1cb29d6db5ae39a85aa6ee6f
none <90691788+Mad-LittleRookie@users.noreply.github.com>
2022-10-27 23:39:58 -0500

    Question 169

---
+++ src/Solution169.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Solution169 {
+    protected class Solution {
+        public int majorityElement(int[] nums) {
+//            Map<String,Integer> answer = new HashMap<String,Integer>();
+//            answer.put("0",0);
+//            Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+//            for( int i: nums){
+//                if(map.containsKey(i)==false){
+//                    map.put(i,0);
+//                }
+//                else{int k = map.get(i);
+//
+//                    map.put(i,k+1);
+//                }
+//            }
+//
+//            return
+            int majorityCount = nums.length/2;
+            int count=0;
+            for(int i:nums){
+                for( int j:nums){
+                    if(j==i){
+                        count+=1;//why I cannot use count++?
+                    }
+
+                }
+                if(count > majorityCount){
+                    return i;
+                }
+                count=0;
+            }
+            return -1;
+        }
+        }
+    }
+
