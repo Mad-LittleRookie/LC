006c68f79d1f6417b9e0f435aaacc1bc608c435f
none <none@no.com>
2022-10-27 21:51:36 -0500

    Question 94

---
+++ src/Solution94.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution94 {
+    protected class Solution {
+        public class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+            TreeNode() {}
+            TreeNode(int val) { this.val = val; }
+            TreeNode(int val, TreeNode left, TreeNode right) {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+    }
+    public List<Integer> inorderTraversal(Solution.TreeNode root) {
+        List<Integer> output = new ArrayList<>();
+        add(root,output);
+        return output;
+    }
+    protected void add(Solution.TreeNode root, List<Integer> output){
+        if(root != null){
+            add(root.left,output);
+            output.add(root.val);
+
+            add(root.right,output);
+        }
+
+    }
+}
