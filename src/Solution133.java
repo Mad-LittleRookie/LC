902a3cdf3666d4b1d7e45a5fbf32642a0755e50a
none <90691788+Mad-LittleRookie@users.noreply.github.com>
2022-10-29 12:16:47 -0500

    Question 133 Unsloved

---
+++ src/Solution133.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Solution133 {
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+    class Solution {
+        Node output = new Node();
+        public Node cloneGraph(Node node) {
+//            ArrayList<Node> array = (ArrayList<Node>) node.neighbors;
+//            Node j = new Node(count,array);
+//            answer.add(j);
+//            count +=1;
+//            if(node==null){
+//                return answer;
+//            }
+//            return cloneGraph(j);
+            for(Node i:node.neighbors){
+                output.neighbors.add(i);
+            }
+            return cloneGraph(output);
+        }
+
+    }
+}
