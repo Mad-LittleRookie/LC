fe66dadd9eea9177b1d5191338fdfa6bd8790294
none <90691788+Mad-LittleRookie@users.noreply.github.com>
2023-02-18 22:57:58 -0600

    Comment

--- src/Solution17.java
+++ src/Solution17.java
@@ -6,7 +6,6 @@ public class Solution17
     class Solution {
 //        public static class number {
 //            String[] number2 = new String[]{"2", "a", "b", "c"};
-//            String[] number3 = new String[]{"3", "d", "e", "f"};
 
 
         private  List<String> combinations = new ArrayList<>();
